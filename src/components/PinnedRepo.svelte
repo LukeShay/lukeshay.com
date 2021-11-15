ae6019160cd08b17dee1f5b662691862e32b6d86
Luke Shay <38476395+LukeShay@users.noreply.github.com>
2021-11-15 09:34:34 -0600

    added automerge action for dependabot (#375)
    * added automerge action for dependabot
    
    * formatted
    
    * added test script
    
    * yarn berry
    
    * added dev container

--- src/components/PinnedRepo.svelte
+++ src/components/PinnedRepo.svelte
@@ -5,7 +5,9 @@
 </script>
 
 <a href="https://github.com/{repo.nameWithOwner}" target="_blank" rel="noopener noreferrer">
-  <div class="w-full hover:ring-4 ring-teal-400 hover:shadow-lg transition duration-300 ease-in-out p-4 rounded min-h-[120px]">
+  <div
+    class="w-full hover:ring-4 ring-teal-400 hover:shadow-lg transition duration-300 ease-in-out p-4 rounded min-h-[120px]"
+  >
     <div class="text-black dark:text-white flex items-center font-semibold text-lg">
       {repo.nameWithOwner}<ExternalLink class="h-5 w-5 ml-2" />
     </div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,14 @@
 import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
 
-/** @type {import('@sveltejs/kit').Config} */
+/**
+ * @type {import('@sveltejs/kit').Config}
+ */
 const config = {
   kit: {
     target: '#svelte',
     adapter: vercel(),
+    vite: { server: { hmr: false } },
   },
   preprocess: [
     preprocess({
