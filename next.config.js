fa71a702a12987e8c1fe2ad3ef658caa48d92fe1
Luke Shay <shay.luke17@gmail.com>
2022-05-19 17:44:45 -0500

    added sitemap

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    generateRobotsTxt: true,
     reactStrictMode: true,
+    // eslint-disable-next-line node/prefer-global/process
+    siteUrl: process.env.SITE_URL || 'https://www.lukeshay.com',
 };
 
 module.exports = nextConfig;
