06c078faf67cfbf73a6b4b98662663adbe29f568
Luke Shay <shay.luke17@gmail.com>
2022-05-19 07:35:55 -0500

    worked on navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,24 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {FC} from 'react';
+import type {AppProps} from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import '../styles/globals.css';
+import {Navigation} from '../components/navigation';
 
-export default MyApp
+const MyApp: FC<AppProps> = ({Component, pageProps}) => (
+    <div className="w-full h-screen bg-slate-200 p-2">
+        <div className="max-w-screen-2xl h-full mx-auto">
+            <div className="w-full h-full flex flex-row">
+                <div className="w-72">
+                    <Navigation />
+                </div>
+                <div className="col-span-9 w-full rounded-md bg-slate-100 overflow-y-scroll">
+                    <div className="max-w-2xl w-full mx-auto px-2 mt-24">
+                        <Component {...pageProps} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+);
+
+export default MyApp;
