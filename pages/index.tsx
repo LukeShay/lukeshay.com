3dc66a3725911814a0895a7a7a2d6c3b43638135
Luke Shay <shay.luke17@gmail.com>
2022-05-20 12:00:23 -0500

    started timeline

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,26 @@
+import {AcademicCapIcon, BriefcaseIcon, CakeIcon} from '@heroicons/react/outline';
 import type {NextPage} from 'next';
+import type {FC, ReactNode} from 'react';
+
+type TimeLineItemProps = {
+    title: string;
+    description: string;
+    date: string;
+    icon: ReactNode;
+};
+
+const TimeLineItem: FC<TimeLineItemProps> = ({title, description, date, icon}) => (
+    <div className="flex items-center">
+        <div className="pr-4">{icon}</div>
+        <div className="flex w-full items-center justify-between border-b border-slate-200 py-5">
+            <div>
+                <h3>{title}</h3>
+                <p className="text-sm text-slate-500">{description}</p>
+            </div>
+            <p className="text-sm text-slate-500">{date}</p>
+        </div>
+    </div>
+);
 
 const HomePage: NextPage = () => (
     <>
@@ -23,9 +45,60 @@ const HomePage: NextPage = () => (
             />
         </div>
         <div>
-            <p>{'Timeline item 1'}</p>
-            <p>{'Timeline item 2'}</p>
-            <p>{'Timeline item 3'}</p>
+            <TimeLineItem
+                date="Dec 2021"
+                description="A short description"
+                icon={<BriefcaseIcon className="h-6 w-6 text-slate-700" />}
+                title="Started Job at Hy-Vee"
+            />
+            <TimeLineItem
+                date="May 2021"
+                description="A short description"
+                icon={<BriefcaseIcon className="h-6 w-6 text-slate-700" />}
+                title="Started Full Time at Rain and Hail Insurance"
+            />
+            <TimeLineItem
+                date="May 2021"
+                description="A short description"
+                icon={<AcademicCapIcon className="h-6 w-6 text-slate-700" />}
+                title="Graduated from Iowa State University"
+            />
+            <TimeLineItem
+                date="Apr 2021"
+                description="A short description"
+                icon={<CakeIcon className="h-6 w-6 text-slate-700" />}
+                title="Got Engaged"
+            />
+            <TimeLineItem
+                date="Nov 2020"
+                description="A short description"
+                icon={<BriefcaseIcon className="h-6 w-6 text-slate-700" />}
+                title="Started Internship at Rain and Hail Insurance"
+            />
+            <TimeLineItem
+                date="May 2020"
+                description="A short description"
+                icon={<BriefcaseIcon className="h-6 w-6 text-slate-700" />}
+                title="Started Internship at Source Allies"
+            />
+            <TimeLineItem
+                date="Oct 2019"
+                description="A short description"
+                icon={<BriefcaseIcon className="h-6 w-6 text-slate-700" />}
+                title="Started Internship at Vertex Software"
+            />
+            <TimeLineItem
+                date="Apr 2019"
+                description="A short description"
+                icon={<BriefcaseIcon className="h-6 w-6 text-slate-700" />}
+                title="Started Internship at Maverick Software Consulting"
+            />
+            <TimeLineItem
+                date="August 2021"
+                description="A short description"
+                icon={<AcademicCapIcon className="h-6 w-6 text-slate-700" />}
+                title="Started College at Iowa State University"
+            />
         </div>
     </>
 );
