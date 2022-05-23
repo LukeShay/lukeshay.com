b22a78f05dd1f9e2fc85869182e5f67b172110ce
Luke Shay <shay.luke17@gmail.com>
2022-05-23 14:06:44 -0500

    added some meta

---
+++ pages/_document.tsx
@@ -0,0 +1,18 @@
+import type {NextPage} from 'next';
+import {Html, Head, Main, NextScript} from 'next/document';
+
+const Document: NextPage = () => (
+    <Html>
+        <Head>
+            <meta content="KkcrsGQ-n47HGC7e8DaENSFU8xwC_JtW1QAZnv3zioM" name="google-site-verification" />
+            <meta content="#f1f5f9" name="theme-color" />
+            <meta content="#f1f5f9" name="msapplication-TileColor" />
+        </Head>
+        <body>
+            <Main />
+            <NextScript />
+        </body>
+    </Html>
+);
+
+export default Document;
