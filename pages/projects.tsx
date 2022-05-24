eea5a51d2a72fb9eb6e387c41f390d3a8a5980f4
Luke Shay <shay.luke17@gmail.com>
2022-05-23 20:47:57 -0500

    start working on projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,30 +1,64 @@
-import type {NextPage} from 'next';
+import process from 'node:process';
 
-const ProjectsPage: NextPage = () => (
+import type {InferGetStaticPropsType, NextPage} from 'next';
+import {graphql} from '@octokit/graphql';
+import {ExternalLinkIcon} from '@heroicons/react/outline';
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type,@typescript-eslint/explicit-module-boundary-types
+const getStaticProps = async () => {
+    const octokit = graphql.defaults({
+        headers: {
+            authorization: `token ${process.env.GITHUB_API_TOKEN ?? ''}`,
+        },
+    });
+
+    const result: {
+        viewer: {
+            pinnedItems: {
+                nodes: {
+                    id: string;
+                    nameWithOwner: string;
+                    description: string;
+                }[];
+            };
+        };
+    } = await octokit(`{
+        viewer { 
+            pinnedItems(first: 6, types: [REPOSITORY]) {
+                nodes {
+                    __typename
+                    ... on Repository {
+                        id
+                        nameWithOwner
+                        description
+                    }
+                }
+            }
+        }
+    }`);
+
+    return {
+        props: {
+            repos: result.viewer.pinnedItems.nodes,
+        },
+        revalidate: 60 * 60 * 24,
+    };
+};
+
+const ProjectsPage: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({repos}) => (
     <>
         <h1 className="pb-6 text-4xl font-bold">{'My Projects'}</h1>
-        <div className="text-slate-700">
-            {
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur auctor lorem nec vehicula dictum. Maecenas a quam gravida, volutpat risus a, fringilla massa. Etiam vitae lacinia ipsum, eu auctor velit. Mauris eget odio sit amet nunc volutpat imperdiet vel a ante. Morbi porttitor odio et massa aliquam tempus. Donec suscipit facilisis arcu ac hendrerit. Aenean nisl augue, porttitor quis porta et, varius eu turpis. Duis pharetra mi sed tristique porta. Mauris hendrerit urna lacus, at maximus magna malesuada sollicitudin. Integer volutpat accumsan sollicitudin. In faucibus ultricies odio, et maximus augue euismod ac. Duis aliquet bibendum neque vitae pulvinar. Suspendisse a justo quis orci pretium venenatis. In non mauris vehicula, convallis nisl a, venenatis dui.'
-            }
-
-            {
-                'Aliquam porta viverra ultricies. Nunc et nisi vitae nisi suscipit tempor in et elit. In vel rhoncus libero. Nullam suscipit odio ac interdum rhoncus. Morbi faucibus scelerisque ante, quis suscipit lacus fringilla vitae. Proin vel leo a ligula ultricies tempus eleifend sed ipsum. Mauris fringilla nisi ex, et porta purus molestie ultrices. Nullam venenatis enim ut ipsum pretium, eget placerat mi blandit. Cras semper auctor tellus, nec suscipit tellus pretium eget.'
-            }
-
-            {
-                'Praesent interdum, arcu at sagittis mollis, nisi justo dictum diam, tempor dignissim ex massa sit amet sem. Nunc vitae condimentum elit. Nulla facilisi. Mauris eget egestas velit, id lacinia arcu. Fusce nunc orci, imperdiet sed ultrices vel, venenatis vel tellus. Morbi varius lorem in nibh varius scelerisque. Donec auctor, massa at maximus ornare, enim lacus varius odio, non convallis felis mi eu neque.'
-            }
-
-            {
-                'Suspendisse porta dapibus metus, vitae egestas erat tincidunt dictum. Praesent nulla purus, sollicitudin eget sem ut, ultricies aliquet nisi. Aenean ornare egestas tortor. Donec ut massa nisi. Maecenas sodales odio a venenatis tincidunt. Pellentesque sed enim sed ex maximus elementum non nec purus. Etiam ut dolor lacus. Suspendisse tempor ut purus at accumsan. Fusce lobortis mauris ut nisl viverra, ac faucibus lectus hendrerit. Maecenas nec enim at tellus tincidunt eleifend.'
-            }
-
-            {
-                'Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum eu accumsan arcu. Fusce rutrum condimentum consectetur. Ut tempus sapien risus, at suscipit eros molestie scelerisque. Curabitur id justo vestibulum, bibendum leo ut, sodales lectus. Morbi eleifend ornare massa, vitae tincidunt ex semper sit amet. Maecenas porta mauris eu tellus euismod scelerisque. Aenean dictum neque vitae urna interdum, id feugiat orci placerat. Nulla nulla lorem, lacinia id eros vitae, accumsan convallis velit. Etiam elit dolor, elementum eget semper eu, efficitur id nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Fusce ut gravida nisl. Vivamus cursus nisl mollis enim ornare, et eleifend quam condimentum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla dapibus consequat tellus vitae scelerisque. Etiam leo arcu, hendrerit eget urna eu, scelerisque egestas arcu.'
-            }
-        </div>
+        {repos.map(({id, nameWithOwner, description}) => (
+            <a href={`https://github.com/${nameWithOwner}`} key={id} rel="noopener noreferrer" target="_blank">
+                <div className="relative my-6 rounded-md border border-slate-300 p-6 duration-300 ease-in-out hover:scale-y-[1.0125] hover:scale-x-[1.0125] hover:border-slate-500">
+                    <ExternalLinkIcon className="absolute top-4 right-4 h-4 w-4 text-slate-500" />
+                    <h2 className="text-lg font-semibold">{nameWithOwner}</h2>
+                    <p className="text-slate-700">{description}</p>
+                </div>
+            </a>
+        ))}
     </>
 );
 
+export {getStaticProps};
 export default ProjectsPage;
