972dd6a0b672b03ac0087e5f57fb892e350b3262
Luke Shay <shay.luke17@gmail.com>
2023-01-19 14:23:00 -0600

    default build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind(),
-		sitemap({
-			customPages: [
-				"https://www.lukeshay.com/",
-				"https://www.lukeshay.com/projects/",
-				"https://www.lukeshay.com/contact/",
-			],
-		}),
-	],
+	integrations: [tailwind()],
 	output: "server",
-	adapter: cloudflare({ mode: "directory" }),
+	adapter: cloudflare(),
 });
