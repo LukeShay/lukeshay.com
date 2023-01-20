126c7c8c27fbe14e587f1c2722297549674ae707
Luke Shay <shay.luke17@gmail.com>
2023-01-20 09:56:58 -0600

    log it

--- src/lib/server/repositories/github-repository.ts
+++ src/lib/server/repositories/github-repository.ts
@@ -29,6 +29,8 @@ export async function getPinnedRepositories() {
 			},
 		});
 
+		console.log("token:", process.env.GITHUB_API_TOKEN, import.meta.env.GITHUB_API_TOKEN);
+
 		const result: {
 			data: {
 				viewer: {
